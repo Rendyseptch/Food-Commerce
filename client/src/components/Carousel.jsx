606a1b3cacdb2d86b853531dcce75803acd91aed
Rendy Septian Pradana <126131312+Rendyseptch@users.noreply.github.com>
2024-12-12 16:56:02 +0700

    commit shop

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+class RenderCarousal extends Component {
+  render() {
+    return (
+      <Carousel interval="3000">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={require("../photos/Carousal1.jpg")}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h1 className="font-bold text-5xl">Welcome to Grown Cook!</h1>
+            <p>
+              Order delicious homemade food from local chefs in Islamabad.
+              Scroll down to see todays menu.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={require("../photos/Carousal2.jpg")}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h1 className=" font-bold text-5xl">Delivery at your Food estate!</h1>
+            <p>Enjoy exceptional taste with delivery time around 5 hours.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+}
+
+export default RenderCarousal;
---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        title:["  Plus Jakarta Sans"]
+    }
+    },
+  },
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        'html, body': {
+          margin: '0',
+          padding: '0',
+          width: '100%',
+        },
+      });
+    },
+  ],
+}
